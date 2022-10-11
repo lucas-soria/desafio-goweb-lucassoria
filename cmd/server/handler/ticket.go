0140517aab28d701ed592c470dba807379f7be5d
lucas-soria <lucas.soria@mercadolibre.com>
2022-10-11 11:02:53 -0300

    FEAT: project structure initialization

---
+++ cmd/server/handler/ticket.go
@@ -0,0 +1,47 @@
+package handler
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/lucas-soria/desafio-goweb-lucassoria/internal/tickets"
+	"net/http"
+)
+
+type Service struct {
+	service tickets.Service
+}
+
+func NewService(s tickets.Service) *Service {
+	return &Service{
+		service: s,
+	}
+}
+
+func (s *Service) GetTicketsByCountry() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		destination := c.Param("dest")
+
+		tickets, err := s.service.GetTotalTickets(c, destination)
+		if err != nil {
+			c.String(http.StatusInternalServerError, err.Error(), nil)
+			return
+		}
+
+		c.JSON(200, tickets)
+	}
+}
+
+func (s *Service) AverageDestination() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		destination := c.Param("dest")
+
+		avg, err := s.service.AverageDestination(c, destination)
+		if err != nil {
+			c.String(http.StatusInternalServerError, err.Error(), nil)
+			return
+		}
+
+		c.JSON(200, avg)
+	}
+}
